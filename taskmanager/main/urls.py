f942212bafeef139981af37852c5dc726b49d16a
Анастасия Пантюшечкина <anastasia.pantushechkina@outlook.com>
2022-11-05 17:45:57 +0300

    !

--- taskmanager/main/urls.py
+++ taskmanager/main/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('contacts', views.contacts, name='contacts'),
     path('catalog/', views.catalog, name='catalog'),
+    path('news/', views.news, name='news'),
+    path('news/news_1/', views.news_1, name='news_1'),
+    path('about/', views.about, name='about'),
+    path('obr_sv/', views.obr_sv, name='obr_sv'),
+    path('gde_kupit/', views.gde_kupit, name='gde_kupit'),
 ]
--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -6,6 +6,26 @@ def index(request):
     return render(request, 'main/index.html')
 
 
+def news(request):
+    return render(request, 'main/news.html')
+
+
+def news_1(request):
+    return render(request, 'main/news_1.html')
+
+
+def obr_sv(request):
+    return render(request, 'main/obr_sv.html')
+
+
+def gde_kupit(request):
+    return render(request, 'main/gde_kupit.html')
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
 def contacts(request):
     contactsList = Contact.objects.all()
     return render(request, 'main/contacts.html', {'contacts': contactsList})
